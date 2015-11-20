106817b77aca33cb77d2849eeda19b1f26b46f45
Garet McKinley <garetmckinley@me.com>
2015-11-20 15:10:09 -0500

    wrapped jService and HPImageArchive in functions

--- app.py
+++ app.py
@@ -9,11 +9,31 @@ app = Flask(__name__)
 # Fetch the secret key from our environment
 app.secret_key = os.environ.get("SECRET_KEY")
 
-# Wrap the jService API
-jService = tortilla.wrap('http://jservice.io/api')
 
-# Wrap the Bing HPImageArchive API
-HPImageArchive = tortilla.wrap('http://www.bing.com/HPImageArchive.aspx')
+def Question():
+    # Wrap the jService API
+    jService = tortilla.wrap('http://jservice.io/api')
+    # Fetch a random trivia question from the jService.io API
+    try:
+        response = jService.random.get(params={'count': 1})[0]
+    except Exception as e:
+        return False
+    # Retreive only the question from the response object
+    question = response['question']
+    return question
+
+
+def BingBackground():
+    # Wrap the Bing HPImageArchive API
+    HPImageArchive = tortilla.wrap('http://www.bing.com/HPImageArchive.aspx')
+    response = HPImageArchive.get(params={
+        'format': 'js',
+        'idx': 0,
+        'n': 1,
+        'mkt': 'en-US'
+    })
+    image = 'http://bing.com/%s' % response['images'][0]['url']
+    return image
 
 
 # This is the class for the home page view
@@ -23,14 +43,7 @@ class AppView(FlaskView):
 
     # This is the function that renders the home page
     def index(self):
-        response = HPImageArchive.get(params={
-            'format': 'js',
-            'idx': 0,
-            'n': 1,
-            'mkt': 'en-US'
-        })
-        image = 'http://bing.com/%s' % response['images'][0]['url']
-        return render_template('index.html', background=image)
+        return render_template('index.html', background=BingBackground())
 # Register the view with our app
 AppView.register(app)
 
@@ -39,14 +52,8 @@ AppView.register(app)
 class SearchView(FlaskView):
     # This is the function that renders the /search/ page
     def index(self):
-        # Fetch a random trivia question from the jService.io API
-        response = jService.random.get(params={'count': 1})[0]
-
-        # Retreive only the question from the response object
-        question = response['question']
-
         # Return the redirect template with the question passed to it
-        return render_template('redirect.html', question=question)
+        return render_template('redirect.html', question=Question())
 # Register the view with our app
 SearchView.register(app)
 
