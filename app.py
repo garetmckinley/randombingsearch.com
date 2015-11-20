a240c4a369db9529e9232355087f5ac6e48f5508
Garet McKinley <garetmckinley@me.com>
2015-11-20 13:08:01 -0500

    commented the fuck out of it

--- app.py
+++ app.py
@@ -1,19 +1,48 @@
 import os
+import tortilla
 from flask import Flask, render_template
 from flask.ext.classy import FlaskView
 
+# Create our Flask app
 app = Flask(__name__)
+
+# Fetch the secret key from our environment
 app.secret_key = os.environ.get("SECRET_KEY")
 
+# Wrap the API
+jService = tortilla.wrap('http://jservice.io/api')
 
+
+# This is the class for the home page view
 class AppView(FlaskView):
+    # Route this view to the root '/' of our url
     route_base = '/'
 
+    # This is the function that renders the home page
     def index(self):
         return render_template('index.html')
-
+# Register the view with our app
 AppView.register(app)
 
+
+# This is the class for the /search/ view.
+class SearchView(FlaskView):
+    # This is the function that renders the /search/ page
+    def index(self):
+        # Fetch a random trivia question from the jService.io API
+        response = jService.random.get(params={'count': 1})[0]
+
+        # Retreive only the question from the response object
+        question = response['question']
+
+        # Return the redirect template with the question passed to it
+        return render_template('redirect.html', question=question)
+# Register the view with our app
+SearchView.register(app)
+
+# If this file is being run and not imported
 if __name__ == '__main__':
+    # Enable debug mode (disable in production)
     app.debug = True
+    # Run our Flask app
     app.run()
