45ede4d5cccc3205c047b82f3f0db4994753f787
Garet McKinley <garetmckinley@me.com>
2015-11-21 00:12:38 -0500

    Added gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     minifyCSS = require('gulp-minify-css'),
     coffee = require('gulp-coffee'),
+    watch = require('gulp-watch'),
     minifyJS = require('gulp-uglify');
 
 gulp.task('default', function() {
     gulp.src('src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(watch('src/sass/*.scss'))
     .pipe(gulp.dest('static/css'));
 
     gulp.src('src/coffee/*.coffee')
     .pipe(coffee())
     .pipe(minifyJS())
+    .pipe(watch('src/coffee/*.coffee'))
     .pipe(gulp.dest('static/js'));
 });
 
